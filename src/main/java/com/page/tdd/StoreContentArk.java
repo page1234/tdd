3517d7665bd223b2379e615de6578f0679452f8a
Page <baiyejianxin@126.com>
2020-03-06 17:11:27 +0800

    [Test 02] 测试绿

--- src/main/java/com/page/tdd/StoreContentArk.java
+++ src/main/java/com/page/tdd/StoreContentArk.java
@@ -1,10 +1,24 @@
 package com.page.tdd;
 
+import com.page.tdd.exception.StoreBagFailException;
+
 public class StoreContentArk {
+
+    private final int space;
+
+    private int hadStoredCount;
+
     public StoreContentArk(int space) {
+        this.space = space;
     }
 
     public QRCode store(Bag bag) {
+        if (hadStoredCount >= space) {
+            throw new StoreBagFailException();
+        }
+
+        hadStoredCount++;
+
         return new QRCode();
     }
 }
