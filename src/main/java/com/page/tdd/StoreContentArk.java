eecca8d2e3271c3def26571459e57fe185a09866
Page <baiyejianxin@126.com>
2020-03-06 17:33:15 +0800

    [Test 03] 测试绿

--- src/main/java/com/page/tdd/StoreContentArk.java
+++ src/main/java/com/page/tdd/StoreContentArk.java
@@ -2,14 +2,20 @@ package com.page.tdd;
 
 import com.page.tdd.exception.StoreBagFailException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StoreContentArk {
 
     private final int space;
 
     private int hadStoredCount;
 
+    private final Map<QRCode, Bag> arks;
+
     public StoreContentArk(int space) {
         this.space = space;
+        arks = new HashMap<>();
     }
 
     public QRCode store(Bag bag) {
@@ -19,10 +25,13 @@ public class StoreContentArk {
 
         hadStoredCount++;
 
-        return new QRCode();
+        QRCode qrCode = new QRCode();
+        arks.put(qrCode, bag);
+
+        return qrCode;
     }
 
     public Bag pickUp(QRCode qrCode) {
-        return null;
+        return arks.get(qrCode);
     }
 }
