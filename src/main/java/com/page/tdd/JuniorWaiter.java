bc22749e1fe424421f2114b8902d531325b35a17
Page <baiyejianxin@126.com>
2020-04-04 17:58:20 +0800

    [JuniorWaiterTest 08] 重构：提炼函数，让原来的方法职责更加单一。

--- src/main/java/com/page/tdd/JuniorWaiter.java
+++ src/main/java/com/page/tdd/JuniorWaiter.java
@@ -1,6 +1,5 @@
 package com.page.tdd;
 
-import com.page.tdd.exception.InvalidQRCodeException;
 import com.page.tdd.exception.StoreBagFailException;
 import com.page.tdd.exception.StoreContentArkFullException;
 
@@ -31,17 +30,18 @@ public class JuniorWaiter {
 
     public Bag pickUp(QRCode qrcode) {
         return (Bag) storeContentArks.stream()
-                .map(storeContentArk -> {
-                    try {
-                        return Optional.ofNullable(storeContentArk.pickUp(qrcode));
-                    } catch (Exception e) {
-                        System.out.println(">>>");
-                        return Optional.empty();
-                    }
-                })
+                .map(storeContentArk -> pickUpFromStoreContentArk(qrcode, storeContentArk))
                 .filter(Optional::isPresent)
                 .findFirst()
                 .map(Optional::get)
                 .orElseGet(null);
+    }
+
+    private Optional<?> pickUpFromStoreContentArk(QRCode qrcode, StoreContentArk storeContentArk) {
+        try {
+            return Optional.ofNullable(storeContentArk.pickUp(qrcode));
+        } catch (Exception e) {
+            return Optional.empty();
+        }
     }
 }
